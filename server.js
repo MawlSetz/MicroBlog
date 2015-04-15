813a48b7815f390d7ca84d707008dcbeb403fe04
Setzer <molly.setzer@gmail.com>
2015-04-15 12:48:26 -0400

    added redirect + show posts to server

---
+++ server.js
@@ -0,0 +1,48 @@
+//State variables + REQUIREMENTS
+
+var express = require('express')
+var app = express();
+
+var ejs = require('ejs')
+app.set('view engine', 'ejs')
+
+var bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended: false}))
+
+var methodOverride = require('method-Override')
+app.use(methodOverride('_method'))
+
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('daapPosts.db');
+
+var request = require('request')
+
+
+app.get('/', function(req, res){
+	res.redirect('/posts')
+});
+//show all posts
+app.get('/posts', function(req, res){
+	db.all('SELECT * FROM posts', function(err, data){
+		if (err) {
+			console.log(err);
+		} else {
+			var posts = data;
+			console.log(posts)
+		} res.render('index.ejs', {posts: data});
+	});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+//ROUTES
